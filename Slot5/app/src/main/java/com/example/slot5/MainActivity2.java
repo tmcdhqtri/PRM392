f651d0b2293fd67be716ec50c0a745ae413e8955
Tran Manh Cuong <cunbeo04012017@gmail.com>
2023-05-24 13:31:23 +0700

    Exercise 6

---
+++ Slot5/app/src/main/java/com/example/slot5/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.slot5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity2 extends AppCompatActivity {
+    TextView tvShow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tvShow = (TextView) findViewById(R.id.tvShow);
+        if (getIntent()!=null) {
+            String data = getIntent().getStringExtra("MESSAGE");
+            tvShow.setText("Hello " + data);
+        }
+    }
+}
