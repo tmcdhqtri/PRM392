1d02de7938bc16cf57182b42cd2b4f845c19792d
Tran Manh Cuong <cunbeo04012017@gmail.com>
2023-06-29 00:02:34 +0700

    slot16

---
+++ Slot16/app/src/main/java/com/example/slot16/DynamicConnectedReceiver.java
@@ -0,0 +1,25 @@
+package com.example.slot16;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+public class DynamicConnectedReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isWifiConnected(context.getApplicationContext())) {
+            Toast.makeText(context, "Wifi is on",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Wifi is off",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    private static boolean isWifiConnected(Context context) {
+        return
+                Settings.System.getInt(context.getContentResolver(),
+                        Settings.Global.WIFI_ON, 0) != 0;
+    }
+}
